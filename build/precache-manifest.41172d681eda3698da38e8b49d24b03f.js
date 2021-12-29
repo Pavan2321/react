ff2b42df617f7b7c6e2d7625aca8ad6af1e9f8af
Pavan2321 <95262587+Pavan2321@users.noreply.github.com>
2021-12-29 11:16:42 +0530

    Add files via upload

---
+++ build/precache-manifest.41172d681eda3698da38e8b49d24b03f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38d6b18839cb2fba05a6c53afc18e18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0771270daa0d99a583ab",
+    "url": "/static/css/2.8e7202f3.chunk.css"
+  },
+  {
+    "revision": "19d4c470a86392505a6a",
+    "url": "/static/css/main.b4d37015.chunk.css"
+  },
+  {
+    "revision": "0771270daa0d99a583ab",
+    "url": "/static/js/2.33282273.chunk.js"
+  },
+  {
+    "revision": "eb60a1ddee9bdaf329d6c80899bb62b7",
+    "url": "/static/js/2.33282273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d4c470a86392505a6a",
+    "url": "/static/js/main.09e006a1.chunk.js"
+  },
+  {
+    "revision": "d8e97f9315e7b60cbfdf",
+    "url": "/static/js/runtime-main.28eb6b7d.js"
+  }
+]);
